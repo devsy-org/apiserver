7ff7463d0a7062d8879e59af310b0b7d4db03287
Samuel K <skevetter@pm.me>
2026-04-18 14:53:49 -0500

    style: add periods to godoc comments in managerfactory

--- pkg/managerfactory/types.go
+++ pkg/managerfactory/types.go
@@ -8,19 +8,19 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-// SharedManagerFactory is the interface for retrieving managers
+// SharedManagerFactory is the interface for retrieving managers.
 type SharedManagerFactory interface {
 	Cluster(cluster string) ClusterClientAccess
 	Management() ManagementClientAccess
 }
 
-// ClusterClientAccess holds the functions for cluster access
+// ClusterClientAccess holds the functions for cluster access.
 type ClusterClientAccess interface {
 	Config(ctx context.Context) (*rest.Config, error)
 	UncachedClient(ctx context.Context) (client.Client, error)
 }
 
-// ManagementClientAccess holds the functions for management access
+// ManagementClientAccess holds the functions for management access.
 type ManagementClientAccess interface {
 	Config() *rest.Config
 	UncachedClient() client.Client
